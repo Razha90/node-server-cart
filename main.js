9877b1482463688a7ed03c5683ddbab412de7f9b
razha90 <razhajamsiksyah@gmail.com>
2024-01-31 10:07:57 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 8000;
+const path = require('path')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/img', express.static(path.join(__dirname, './public/img')))
+
+const db = require('./app/models/index')
+db.mongoose.connect(db.url)
+    .then(() => {
+        console.log("Connected to the database");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome To Server Cart'
+    })
+})
+
+require('./app/routes/product.route')(app);
+require('./app/routes/order.route')(app);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
